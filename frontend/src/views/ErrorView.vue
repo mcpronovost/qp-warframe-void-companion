d5ee06c10268c2452257e819096d61854209bf78
M-C Pronovost <mariechristine@d-modules.com>
2022-12-19 16:27:59 -0500

    build

---
+++ frontend/src/views/ErrorView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+    <qp-page>
+        <div class="qp-container qp-centered">
+            <el-row>
+                <el-col style="text-align:center;">
+                    <el-result icon="error" :title="$t('ContentNotFound')">
+                        <template #extra>
+                            <el-button @click="$router.push({name:'Home'})">
+                                <span v-text="$t('BackToHome')"></span>
+                            </el-button>
+                        </template>
+                    </el-result>
+                </el-col>
+            </el-row>
+        </div>
+    </qp-page>
+</template>
